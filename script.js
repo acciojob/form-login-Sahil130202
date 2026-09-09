da410eda820f94cf247729412ee94863e154222b
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-09 10:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault()
+	let firstname =  document.querySelector('[name="fname"]').value
+	let secondname =  document.querySelector('[name="lname"]').value
+
+	alert(firstname + " " + secondname)
+	
 
 }
